aa07e620c88a8fd6182728bda037ff1516a54a86
joelmasters <joelmasters@gmail.com>
2019-08-16 07:16:42 -0700

    Updates

---
+++ precache-manifest.c78b4b6387b94b09798c9be0b33f39dc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fed234a04f3f1b9188ae5abbe5cd696f",
+    "url": "/dianes-budget-app/index.html"
+  },
+  {
+    "revision": "7535033ffec797ddeb6c",
+    "url": "/dianes-budget-app/static/css/main.9b55a142.chunk.css"
+  },
+  {
+    "revision": "78ea2df0ccad551b9df8",
+    "url": "/dianes-budget-app/static/js/2.8f71839d.chunk.js"
+  },
+  {
+    "revision": "7535033ffec797ddeb6c",
+    "url": "/dianes-budget-app/static/js/main.0eb79e2b.chunk.js"
+  },
+  {
+    "revision": "d7280b8b0998c2583ae1",
+    "url": "/dianes-budget-app/static/js/runtime~main.45cb375f.js"
+  }
+]);
